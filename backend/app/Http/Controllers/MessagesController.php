f0e62fa590d5daf5a6842c3a7258aa01762e1760
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-03-25 01:43:41 +0200

    Implemented send message

--- backend/app/Http/Controllers/MessagesController.php
+++ backend/app/Http/Controllers/MessagesController.php
@@ -4,8 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Message;
+use Auth;
 
 class MessagesController extends Controller
 {
-    //
+
+    function sendMessage(Request $request){
+        $sender_id = Auth::user()->id;
+        
+        $new_message = new Message;
+        $new_message->message_content = $request->message_content;
+        $new_message->sender_id = $sender_id;
+        $new_message->receiver_id = $request->receiver_id;
+        $new_message->save();
+
+        return response()->json([
+            "success" => true
+        ]);
+    } 
+        
+    
+    
 }
