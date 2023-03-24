6ee627733a48d0c99978e9c9064ab9032808e744
Fadi <fadi@Fadis-MacBook-Pro.local>
2023-03-25 00:13:13 +0200

    add get messages

--- backend/app/Http/Controllers/MessagesController.php
+++ backend/app/Http/Controllers/MessagesController.php
@@ -4,8 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Message;
+use Illuminate\Support\Facades\Validator;
 
 class MessagesController extends Controller
 {
-    //
+    public function getMessages(Request $request){
+        $validator = Validator::make($request->all(), [
+            'receiver_id' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'failed',
+                'message' => $validator->errors()
+            ], 401);
+        }
+    }
 }
