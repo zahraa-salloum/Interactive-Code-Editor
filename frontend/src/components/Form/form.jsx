369dc87b2b3866bd4fb707f1323c3c24558d731c
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-03-26 03:21:11 +0300

    Edited Form Css

--- frontend/src/components/Form/form.jsx
+++ frontend/src/components/Form/form.jsx
@@ -5,7 +5,7 @@ const Form = () => {
     return(
         <div className="form_container">
         <h1>Sign In</h1>
-        <Partition Lname={"Email"} Iname={"email"} />
+        <Partition Lname={"Email"} Iname={"hola"} />
         <Partition Lname={"password"} />
         <RegisterButton name={"Login"}/>
         </div>
