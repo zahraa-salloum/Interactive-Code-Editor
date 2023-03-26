e2530f10df2b06d75bbfe0262bbb38e8fcbd617b
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-03-26 21:22:08 +0300

    Added flex container to the codes previews

--- frontend/src/Pages/CodesPage/index.js
+++ frontend/src/Pages/CodesPage/index.js
@@ -3,6 +3,7 @@ import NavBar from '../../components/NavBar/Nav';
 import Footer from '../../components/Footer/footer';
 import axios from 'axios';
 import CodePreview from '../../components/CodePreview';
+import './style.css'
 
 
 const CodesPage = () => {
@@ -12,7 +13,7 @@ const CodesPage = () => {
     
 
     // const token = window.localStorage.getItem('token');
-    const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vMTI3LjAuMC4xOjgwMDAvYXBpL3YwLjAuMS9hdXRoL2xvZ2luIiwiaWF0IjoxNjc5ODQ1OTEyLCJleHAiOjE2Nzk4NDk1MTIsIm5iZiI6MTY3OTg0NTkxMiwianRpIjoiZmhVOEk3aloySkVuZUtxZiIsInN1YiI6IjIiLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.5ZjAAOga8QoBNAGF6bzY6sZRwE9E4g44P2_HdJTiUdw"
+    const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vMTI3LjAuMC4xOjgwMDAvYXBpL3YwLjAuMS9hdXRoL2xvZ2luIiwiaWF0IjoxNjc5ODUzMzcwLCJleHAiOjE2Nzk4NTY5NzAsIm5iZiI6MTY3OTg1MzM3MCwianRpIjoicFBnVUVDZHRjcHlvU2l0eSIsInN1YiI6IjIiLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.iF_gG08hVY_j8rAv_vHRiYwBkflJmN29vMXxwCYu4BY"
 
     useEffect(() => {
         const getCodes = () => {
@@ -32,6 +33,7 @@ return(
     <div>
         
         <NavBar/>
+        <div className='container_flex_code'>
         {messages.map(message => (
             
             <CodePreview 
@@ -40,6 +42,7 @@ return(
         
 
            ))}
+           </div>
         
         
         
