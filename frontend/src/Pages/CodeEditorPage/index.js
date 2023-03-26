bbb1a29bc71f4fee3f25392198e404ae69db009c
aliynajjar <ali.y.alnajjar@gmail.com>
2023-03-26 16:28:21 +0300

    Added styling in CodeEditorPage

--- frontend/src/Pages/CodeEditorPage/index.js
+++ frontend/src/Pages/CodeEditorPage/index.js
@@ -1,6 +1,8 @@
 import React, {useState} from "react";
 import Editor from "../../components/Editor/Editor.js";
 import Output from "../../components/Editor/Output.js";
+import NavBar from "../../components/NavBar/Nav.jsx";
+import Footer from "../../components/Footer/footer.jsx";
 import axios from "axios";
 
 const CodeEditorPage = () => {
@@ -36,6 +38,11 @@ const CodeEditorPage = () => {
   };
 
   return (
+    <div>
+      <NavBar />
+    <div className="code_editor_page">
+      
+      <button className ="compile_btn" onClick={pythonCompile}>Run</button>
     <div className="container_code_editor">
       <div className="pane top-pane">
         <Editor
@@ -46,11 +53,58 @@ const CodeEditorPage = () => {
         />
       </div>
       <div className="pane top_pane">
-        <Output value={code_output} />
+        <Output 
+        displayName="Output"
+        value={code_output} />
       </div>
-      <button onClick={pythonCompile}>click me to compile</button>
+      
+    </div>
+    </div>
+    <Footer />
     </div>
   );
 };
 
 export default CodeEditorPage;
+
+
+// import React, { useState , useEffect } from "react";
+// import Editor from "../../components/Editor/Editor.js";
+// import Output from "../../components/Editor/Output.js";
+// import { loadPyodide } from 'pyodide';
+
+// const CodeEditorPage = () => {
+//   const [python, setPython] = useState("");
+//   const [code_output, setOutput] = useState("");
+
+//   const handleCodeChange = async() =>{
+//     pythonCompile().then(response => console.log(response));
+//   }
+
+
+//   const pythonCompile = async () => {
+//     const python_code = python;
+//     const pyodide = await window.loadPyodide();
+//     const output = await pyodide.runPythonAsync(python_code);
+//     setOutput(output);
+
+//   };
+//   return (
+//     <div className="container_code_editor">
+//       <div className="pane top_pane">
+//         <Editor
+//           language="python"
+//           displayName="Input"
+//           value={python}
+//           onChange={setPython}
+//         />
+//       </div>
+//       <div className="pane top_pane">
+//       <Output value = {code_output} />
+//       </div>
+//       <button onClick={handleCodeChange}>click me to compile</button>
+//     </div>
+//   );
+// };
+// export default CodeEditorPage;
+
--- frontend/src/components/Editor/Output.js
+++ frontend/src/components/Editor/Output.js
@@ -1,6 +1,23 @@
-const Output = ({value}) => {
-  return (
-          <p>{value}</p>
-  );
-}
+
+
+const Output = (props) => {
+  const {
+    displayName,
+    value,
+} = props
+
+  return(
+    <>
+        <div className="output_container">
+        <div className="output_title">
+          {displayName}
+        </div>
+        <div className="output_value">
+        {value}
+        </div>
+        
+        </div>
+        </>
+    )
+    }
 export default Output;
