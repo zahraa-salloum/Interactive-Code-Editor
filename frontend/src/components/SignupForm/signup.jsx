0d05bd44bb2b70b6564f047d4bf09cb6700846c5
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-03-28 00:03:53 +0300

    Added validations in registeration

--- frontend/src/components/SignupForm/signup.jsx
+++ frontend/src/components/SignupForm/signup.jsx
@@ -1,7 +1,6 @@
 import Partition from "../Partition/partition";
 import  '../../components/LoginForm/loginform.css';
 import RegisterButton from "../RegisterButton/registerbutton";
-import Button from "../Button/button";
 import {useState} from "react";
 import axios from "axios"
 import { useNavigate } from "react-router-dom";
@@ -10,12 +9,21 @@ const [first,setFirst]=useState("")
 const [last,setLast]=useState("")
 const [email,setEmail]=useState("")
 const [password,setPassword]=useState("")
+const [error,setError]=useState("")
 
 const navigator = useNavigate();
 const signinNavigation = () => {
     navigator('/login');
-    
 }
+const validateEmail=(email) =>{
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);}
+
+const validatePassword=(password)=> {
+        const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+        return passwordRegex.test(password);
+      }
+      
 
 const handleFirst=(e)=>{
    setFirst(e.target.value)
@@ -30,6 +38,11 @@ const handlePassword=(e)=>{
     setPassword(e.target.value)
 }
 const handleSubmit=()=>{
+if (first=="" || last=="" || email=="" || password==""){
+    setError("All input are required")}else{
+
+    if (validateEmail(email)){
+        if(validatePassword(password)){
 const data = new FormData()
 data.append("first_name",first)
 data.append("last_name",last)
@@ -37,11 +50,15 @@ data.append("email",email)
 data.append("password",password)
 data.append("user_type_id",2)
 axios.post("http://127.0.0.1:8000/api/v0.0.1/register",data).then((res) => {
-    console.log(res.data);
+    console.log(res)
+    localStorage.setItem('token',res.data.authorisation.token);
+    window.location.href="http://localhost:3000/code_editor"     
 }).catch((err) => {
     console.log(err);
 })
-}
+}else(setError("Invalid credentials"))
+}else(setError("Invalid credentials"))
+    }}
 return(
     <div className="form_container">
          <h1 className="form_title"> Kindly fill the form</h1>
@@ -51,7 +68,8 @@ return(
          <Partition Lname={"Password"} Itype={"password"} onChange={handlePassword}/>
          <RegisterButton name={"Sign up"} onSubmit={handleSubmit}/>
          <br />
-         <p onClick={signinNavigation}>already have an account?</p>
+         <p className="error">{error}</p>
+         <p className="error" onClick={signinNavigation}>already have an account?</p>
  </div>
 )
 }
