f9c91e49acb1fb215410d3fef293b6af5db5ee7a
Fadi <fadi@Fadis-MacBook-Pro.local>
2023-03-27 22:33:13 +0300

    Filtered data coming from the api

--- frontend/src/components/Chats/ChatsList/index.jsx
+++ frontend/src/components/Chats/ChatsList/index.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 
-const ChatsList = ({list}) => {
+const ChatsList = ({list, clicked}) => {
+const existing_user =[]
+list.forEach((data) => {
+    const existing = existing_user.find((user) => user.email === data.email);
+    if (!existing) {
+        existing_user.push(data);
+      }
+})
+
+console.log(existing_user)
+
     return(
          <div className='chat_list_content'>
             <div className='chat_list_header'>Messages</div>
             <div className='chat-search'><input placeholder='Search...'></input></div>
             <div className="chat_list">
-            {list.map((chat,index) => (
+            {existing_user.map((chat,index) => (
                             <React.Fragment key={index}>
-                           <div className="chat_list_image" key={`image-${index}`}></div>
-                           <div className="Chat-name" key={`name-${index}`}>{chat.first_name} {chat.last_name}</div>
+                           {/* <div className="chat_list_image" key={`image-${index}`}></div> */}
+                           <div className="chat_name" key={`name-${index}`} onClick={clicked}>{chat.first_name} {chat.last_name}</div>
                             </React.Fragment>
                         ))}
             </div>
