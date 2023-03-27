3fa6c28389567ef02b0652b7a923e42bbc8b0a4e
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-03-27 04:04:16 +0300

    Edited loging process

--- frontend/src/components/Form/form.jsx
+++ frontend/src/components/Form/form.jsx
@@ -2,30 +2,40 @@ import Partition from "../Partition/partition";
 import  './form.css';
 import RegisterButton from "../RegisterButton/registerbutton";
 import { useEffect, useState } from "react";
+import axios from "axios"
+
 const Form = () => {
 
 const [email, setEmail] = useState("");
-
-console.log(email)
 const [password, setPassword] = useState("");
 const handleEmail=(e)=>{
-    setTimeout(() => {
-        setEmail(e.target.value)
-      }, 5000);
-    
+      setEmail(e.target.value)
   }
  const handlePassword=(e)=>{
-    setTimeout(() => {
-        setPassword(e.target.value)
-      }, 5000);
+     setPassword(e.target.value)
+  }
+  
+  const handleSubmit=()=>{
+    const data = new FormData()
+    data.append("email",email)
+    data.append("password",password )
+    axios.post("http://127.0.0.1:8000/api/v0.0.1/auth/login",data).then((res) => {
+        console.log(res);
+    }).catch((err) => {
+        console.error("Error response:");
+        console.error(err.response.data.message);  
+        console.error(err.response.status);
+        console.log(err);
+    })
+
   }
 
     return(
         <div className="form_container">
         <h1>Sign In</h1>
-        <Partition Lname={"Email"} Itype={"text"} onChange={handleEmail} size="40"/>
+        <Partition Lname={"Email"} Itype={"text"} onChange={handleEmail} size="40" />
         <Partition Lname={"password"} Itype={"password"} onChange={handlePassword}/>
-        <RegisterButton name={"Login"}/>
+        <RegisterButton name={"Login"} onSubmit={handleSubmit}/>
         </div>
     );}
 
